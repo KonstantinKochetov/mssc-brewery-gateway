454e7b42746111d7bcd56a37157b60aa45342ee0
konstantin kochetov <kochetov0824@gmail.com>
2020-07-04 14:55:58 +0200

    local host routing

---
+++ src/main/java/com/konstantinkochetov/msscbrewerygateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,29 @@
+package com.konstantinkochetov.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+//@Profile("!local-discovery")
+@Configuration
+public class LocalHostRouteConfig {
+
+    // take a look (matchers): https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/util/AntPathMatcher.html
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("http://localhost:8080")
+                        .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082")
+                        .id("inventory-service"))
+                .build();
+    }
+}
