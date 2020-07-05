15ce77943270048d0d97dfaf9fbc16d47bba546c
konstantin kochetov <kochetov0824@gmail.com>
2020-07-05 14:28:59 +0200

    routing

--- src/main/java/com/konstantinkochetov/msscbrewerygateway/config/GoogleConfig.java
+++ src/main/java/com/konstantinkochetov/msscbrewerygateway/config/GoogleConfig.java
@@ -3,8 +3,6 @@ package com.konstantinkochetov.msscbrewerygateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 // just an example to play with
 
